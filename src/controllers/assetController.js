7cfd87d402d2e02df7632cb5176229607c5fa8fa
Zetsuboii <hmzkrb@gmail.com>
2021-08-05 22:47:14 +0300

    getAssets handlers

--- src/controllers/assetController.js
+++ src/controllers/assetController.js
@@ -62,14 +62,84 @@ export async function addCollection(collectionDetails) {
 
 /**
  * Gets all collections' details
- * @returns {Promise<[
- *  collectionId: number, userId: number, collectionName: string, 
- *  description: string, logoLink: string, resultCode: number
- * ]>} Details of all collections
+ * @returns {Promise<[{
+ *      collectionId: number, 
+ *      userId: number, 
+ *      collectionName: string, 
+ *      description: string, 
+ *      logoLink: string, 
+ *      resultCode: number
+ * }]>} Details of all collections
  */
 export async function getAllCollections() {
     const response = await fetch(
         baseUrl + '/getAllCollections',
+        {
+            method: 'GET'
+        }
+    );
+
+    return response.json();
+}
+
+/**
+ * Gets all assets
+ * @returns {Promise<[{
+ *      assetId: number, 
+ *      creatorId: number,
+ *      ownerId: number, 
+ *      collectionId: number, 
+ *      categoryId1: number, 
+ *      categoryId2: number, 
+ *      categoryId3: number, 
+ *      ownerAddress: string, 
+ *      type: enum, 
+ *      numberOfCopies: number, 
+ *      title: string, 
+ *      description: string, 
+ *      nftAddress: string, 
+ *      fileLink: string?, 
+ *      previewLink: string,  
+ *      provider: string, 
+ *      size: number, 
+ *      payload: enum, 
+ *      createDate: Date, 
+ *      isMinted: boolean, 
+ *      mintDate: Date, 
+ *      onSale: boolean, 
+ *      state: enum, 
+ *      lastTransferDate: Date, 
+ *      isDeleted: boolean, 
+ *      isActive: boolean, 
+ *      royalty: number, 
+ *      likeCount: number, 
+ *      creationFee: numeric | bigint, 
+ *      resultCode: number
+ * }]>} all assets
+ */
+export async function getAllAssets() {
+    const response = await fetch(
+        baseUrl + '/getAllAssets',
+        {
+            method: 'GET'
+        }
+    );
+
+    return response.json();
+}
+
+/**
+ * Gets on sale assets
+ * @returns {Promise<[{
+ *      assetId: number, 
+ *      title: string, 
+ *      previewLink: string, 
+ *      resultCode: number
+ * }]>}
+ */
+export async function getOnSaleAssets() {
+    const response = await fetch(
+        baseUrl + '/getAllOnSaleAssets',
         {
             method: 'GET'
         }
