91fc375d78f21e55eb3857e43b2536961f2e5907
Zetsuboii <hmzkrb@gmail.com>
2021-08-05 19:16:51 +0300

    Add ternary for displaying collections & assets

--- src/components/views/AssetsPage.jsx
+++ src/components/views/AssetsPage.jsx
@@ -8,6 +8,7 @@ import AddCollection from "../AddCollection";
 
 export default function AssetsPage() {
     const [collections, setCollections] = useState([]);
+    const [assets, setAssets] = useState([])
 
     const [showAddAsset, setShowAddAsset] = useState(false);
     const hAddAsset = () => setShowAddAsset(!showAddAsset);
@@ -29,13 +30,35 @@ export default function AssetsPage() {
 
     return (
         <>
-            {collections.map((collection) => (
-                <div key={collection.collectionId}>
-                    <p>ID: {collection.collectionId}</p>
-                    <h3>Name: {collection.collectionName}</h3>
-                    <p>Description: {collection.description}</p>
-                </div>
-            ))}
+            <h3>Collections: </h3>
+            {
+                collections.length > 0
+                    ? collections.map((collection) => (
+                        <div key={collection.collectionId}>
+                            <p>ID: {collection.collectionId}</p>
+                            <h3>Name: {collection.collectionName}</h3>
+                            <p>Description: {collection.description}</p>
+                        </div>
+                    ))
+                    : (
+                        <div>
+                            <p>None</p>
+                        </div>
+                    )
+            }
+
+            <h3>Assets: </h3>
+            {
+                assets.length > 0
+                    ? (
+                        <p>Assets</p>
+                    )
+                    : (
+                        <div>
+                            <p>None</p>
+                        </div>
+                    )
+            }
 
             <button onClick={hAddCollection}>{showAddCollection ? "Close Modal" : "Add Collection"}</button>
             {showAddCollection && <AddCollection />}
