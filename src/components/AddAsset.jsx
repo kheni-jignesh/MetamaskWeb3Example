e0457ef15543d8355e2793ae752aa7091a3e773d
Zetsuboii <hmzkrb@gmail.com>
2021-08-05 19:04:16 +0300

    refactor addAsset, make it more readable

--- src/components/AddAsset.jsx
+++ src/components/AddAsset.jsx
@@ -1,47 +1,95 @@
 import { useState } from "react";
 
+/**
+ * 
+ * @dev Şu an sadece test amaçlı bu şekilde,
+ * son halinde:
+ * collectionId seçili collection'dan
+ * creatorId userId'den,
+ * categoryId1 & 2 & 3  seçili kategorilerden gelecek
+ * 
+ * ? nftId ve royalty nasıl seçilecek bilmiyorum
+ */
+
 export default function AssetForm() {
-    const formFields = {
-        nftId: 0,
-        creatorId: 0,
-        collectionId: 0,
-        categoryId1: 0,
-        categoryId2: 0,
-        categoryId3: 0,
-        type: "",
-        numberOfCopies: 1,
-        title: "",
-        description: "",
-        royalty: 0,
+    const defaults = {
+        nftId: 0, creatorId: 0, collectionId: 0,
+        categoryId1: 0, categoryId2: 0, categoryId3: 0,
+        type: "", numberOfCopies: 1, title: "",
+        description: "", royalty: 0,
     };
-    const [formData, setFormData] = useState(formFields);
-    const handlers = {
-        "nftId": (e) => setFormData({ ...formData, nftId: e.target.value }),
-        "creatorId": (e) => setFormData({ ...formData, creatorId: e.target.value }),
-        "collectionId": (e) => setFormData({ ...formData, creatorId: e.target.value }),
-        "categoryId1": (e) => setFormData({ ...formData, categoryId1: e.target.value }),
-        "categoryId2": (e) => setFormData({ ...formData, categoryId2: e.target.value }),
-        "categoryId3": (e) => setFormData({ ...formData, categoryId3: e.target.value }),
-        "type": (e) => setFormData({ ...formData, type: e.target.value }),
-        "numberOfCopies": (e) => setFormData({ ...formData, numberOfCopies: e.target.value }),
-        "title": (e) => setFormData({ ...formData, title: e.target.value }),
-        "description": (e) => setFormData({ ...formData, description: e.target.value }),
-        "royalty": (e) => setFormData({ ...formData, royalty: e.target.value })
-    };
+
+    const [nftId, setNftId] = useState(defaults.nftId);
+    const [creatorId, setCreatorId] = useState(defaults.creatorId);
+    const [collectionId, setCollectionId] = useState(defaults.collectionId);
+    const [categoryId1, setCategoryId1] = useState(defaults.categoryId1);
+    const [categoryId2, setCategoryId2] = useState(defaults.categoryId2);
+    const [categoryId3, setCategoryId3] = useState(defaults.categoryId3);
+    const [type, setType] = useState(defaults.type);
+    const [numberOfCopies, setNumberOfCopies] = useState(defaults.numberOfCopies);
+    const [title, setTitle] = useState(defaults.title);
+    const [description, setDescription] = useState(defaults.description);
+    const [royalty, setRoyalty] = useState(defaults.royalty);
+
+    const handleFormData = (fx) => (e) => fx(e.target.value);
 
     return (
         <>
-            <AssetField name="NFT ID" type="number" value={formData.nftId} handler={handlers.nftId} />
-            <AssetField name="Creator ID" type="number" value={formData.creatorId} handler={handlers.creatorId} />
-            <AssetField name="Collection ID" type="number" value={formData.collectionId} handler={handlers.collectionId} />
-            <AssetField name="Category ID 1" type="number" value={formData.categoryId1} handler={handlers.categoryId1} />
-            <AssetField name="Category ID 2" type="number" value={formData.categoryId2} handler={handlers.categoryId2} />
-            <AssetField name="Category ID 3" type="number" value={formData.categoryId3} handler={handlers.categoryId3} />
-            <AssetField name="Type" type="text" value={formData.type} handler={handlers.type} />
-            <AssetField name="Number of Copies" type="number" value={formData.numberOfCopies} handler={handlers.numberOfCopies} />
-            <AssetField name="Title" type="text" value={formData.title} handler={handlers.title} />
-            <AssetField name="Description" type="text" value={formData.description} handler={handlers.description} />
-            <AssetField name="Royalty" type="number" value={formData.royalty} handler={handlers.royalty} />
+            <AssetField name="NFT ID"
+                type="number"
+                value={nftId}
+                handler={handleFormData(setNftId)} />
+
+            <AssetField name="Creator ID"
+                type="number"
+                value={creatorId}
+                handler={handleFormData(setCreatorId)} />
+
+            <AssetField name="Collection ID"
+                type="number"
+                value={collectionId}
+                handler={handleFormData(setCollectionId)} />
+
+            <AssetField name="Category ID 1"
+                type="number"
+                value={categoryId1}
+                handler={handleFormData(setCategoryId1)} />
+
+            <AssetField name="Category ID 2"
+                type="number"
+                value={categoryId2}
+                handler={handleFormData(setCategoryId2)} />
+
+            <AssetField name="Category ID 3"
+                type="number"
+                value={categoryId3}
+                handler={handleFormData(setCategoryId3)} />
+
+            <AssetField name="Type"
+                type="text"
+                value={type}
+                handler={handleFormData(setType)} />
+
+            <AssetField name="Number of Copies"
+                type="number"
+                value={numberOfCopies}
+                handler={handleFormData(setNumberOfCopies)} />
+
+            <AssetField name="Title"
+                type="text"
+                value={title}
+                handler={handleFormData(setTitle)} />
+
+            <AssetField name="Description"
+                type="text"
+                value={description}
+                handler={handleFormData(setDescription)} />
+
+            <AssetField name="Royalty"
+                type="number"
+                value={royalty}
+                handler={handleFormData(setRoyalty)} />
+
             <button>Create Asset</button>
         </>
     )
