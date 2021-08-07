2487b7ac68a1b48ac22eb1854348f5c658a5654d
Zetsuboii <hmzkrb@gmail.com>
2021-08-07 11:02:47 +0300

    update all queries to reutrn nullable and update it to latest backend changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,47 +2,70 @@ import axios from "axios";
 
 const baseUrl = "http://104.248.138.246:8085";
 
-/*
+/**
+ * Throws an error if query result code is 0
+ * @param {any} query 
+ * @param {string} name  
+ */
+const checkQuery = (query, name) => { if (query.data.resultCode === 0) throw new Error(name + " query failed") }
+
+/** 
  * @param {string} walletAddress 
- * @returns {Promise<string | null>} userId
+ * @returns {Promise<{ resultCode: 0 | 1, response: string}?>} user ID
  */
 export async function getUserByWalletAddress(walletAddress) {
     try {
-        const response = await axios.get('/getUserIdByWalletAddress/' + walletAddress, {
+        const query = await axios.get('/getUserIdByWalletAddress/' + walletAddress, {
             baseURL: baseUrl,
         });
 
-        return response;
-    } catch (err) {
+        checkQuery(query, "getUserByWalletAddress");
+        return query.data;
+    }
+    catch (err) {
         console.log(err);
-        return { status: 404 }
+        return null;
     }
 }
 
 /**
  * Gets user details with ID
  * @param {number} id 
- * @returns {Promise<any>} userDetails
+ * @returns {Promise<any?>} userDetails
  */
 export async function getUserById(id) {
-    const response = await axios.get('/getUserById/' + id, {
-        baseURL: baseUrl
-    });
+    try {
+        const query = await axios.get('/getUserById/' + id, {
+            baseURL: baseUrl
+        });
 
-    return response;
+        checkQuery(query, "getUserById");
+        return query.data;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
 }
 
 /**
  * Adds user to DB and retrieve user ID
  * @param {string} walletAddress 
- * @returns {Promise<any>} userId
+ * @returns {Promise<{ resultCode: 0 | 1, id: number }?>} userId
  */
 export async function addUser(walletAddress) {
     const data = { walletAddress };
 
-    const response = await axios.post('/addUser', data, {
-        baseURL: baseUrl
-    });
+    try {
+        const query = await axios.post('/addUser', data, {
+            baseURL: baseUrl
+        });
 
-    return response;
+        checkQuery(query, "addUser")
+        return query.data;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
 }
